47cb8cdbfbcc1afc5f3a2aa969d53f60359b40aa
github username <acc.tung@gmail.com>
2017-10-11 17:06:40 -0400

    cleaned matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -28,48 +28,6 @@ class MatchesController < ApplicationController
     render json: status
   end
 
-  # def show
-  #   current_match = Match.find_by(id: params[:id])
-  #   if current_match.users.find_by(your_turn?:true)
-  #     my_turn = true
-  #   else
-  #     my_turn = false
-  #   end
-  #   status = {
-  #     users: current_match.users,
-  #     started: current_match.started,
-  #     sketch: current_match.sketch ? current_match.sketch : "",
-  #     current_turn: current_match.users.find_by(your_turn?: true)
-  #   }
-  #
-  #   render json: status
-  # end
-
-
-  # def update
-    # #starts the match
-    # current_match = Match.find_by(id: params[:id])
-    # current_match.started = true
-    # current_match.gen_answer
-    # current_match.save
-    # current_user = current_match.users.find_by(id: params[:currentUserID])
-    # current_user[:your_turn?] = true
-    # current_user.save
-    #
-    # render json: current_match
-  # end
-
-  # def turn_end
-    # current_user = User.find_by(id: params[:currentUserID])
-    # next_user = current_user.end_turn
-    #
-    # render json: next_user
-  # end
-
-  # def handle_guess
-    # current_match = Match.find_by(id: params[:id])
-    # guess = params[:guess]
-  # end
 
 
 
